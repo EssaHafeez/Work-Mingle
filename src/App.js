19e2e9f5ebb3a4509d148e448191d80350371d97
Essa Hafeez <hafeezessa11@gmail.com>
2024-08-09 23:40:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import UserInterface from './components/UserInterface/UserInterface';
+function App() {
+  return (
+    <div className="App">
+      <UserInterface/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserInterface/UserInterface.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './UserInterface.css';  // Importing the CSS file
+import logo from '../../Images/logo.png';
+import headerlogo from '../../Images/headerlogo.png';
+
+
+function UserInterface() {
+
+    return (
+        <div className="work-mingle-container">
+            <header className="header">
+                <img src={logo}  alt="Work Mingle Logo" className="logo" />
+                <button className="sign-up-button">Sign up →</button>
+            </header>
+
+            <div className="content">
+                <div className="text-content">
+                    <h1>Empowering laborers,
+                        connecting customers</h1>
+                    <p>Tired of the headache of hiring laborers? Look no further than Work Mingle. 
+                        Our platform offers a seamless solution for connecting customers with trustworthy
+                        painters and cleaners.  With real-time communication, geolocation services, and 
+                        secure payment options, Work Mingle makes hiring easy, efficient, and stress-free. 
+                        Join us today and experience the difference.</p>
+                    <button className="start-button">Start today →</button>
+                </div>
+                <div className="image-content">
+                    <img src={headerlogo} alt="Laborers Tools" className="tools-image" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserInterface;
