536795e42be98600366c1b7eaf97941598dbc2b2
MuhammadEssabawag <essahafe60003@gmail.com>
2024-08-12 21:38:28 +0500

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import UserInterface from './components/UserInterface/UserInterface';
+import SelectCategory from './Pages/SelectCategory/SelectCategory';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      <UserInterface/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserInterface />} />
+        <Route path="/category" element={<SelectCategory />} />
+      </Routes>
+    </Router>
   );
 }
 
