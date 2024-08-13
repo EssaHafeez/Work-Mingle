ecb49402bd4572152bad84314bcd39f3726a7485
MuhammadEssabawag <essahafe60003@gmail.com>
2024-08-13 16:39:57 +0500

    href-later-edit

--- src/Pages/Login-Signup/Login-Signup.jsx
+++ src/Pages/Login-Signup/Login-Signup.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import './Login-Signup.css';
-import { useNavigate } from 'react-router-dom';
 
 function LoginSignup() {
   return (
     <div className="select-category-container">
       <div className="select-category-box">
         <div className="select-category-title">
-          <h1>JoinUsAs</h1>
+          <h1>WorkMingle</h1>
         </div>
         <div className="login-signup-v2">
           <div className="login-signup-v2-button-first">
-          <button className="css-u25o92">
-              <span className='labour'>Labour</span>
-            </button> 
-          </div>
-          <div className="login-signup-v2-buttons">
-          <button className="css-1vj7isi">
-              <span >Customer</span>
+            <button className="css-u25o92">
+              <span className="labour">Login</span>
             </button>
           </div>
-         
+          <div className="login-signup-v2-buttons">
+            <button className="css-1vj7isi">
+              <span>Signup</span>
+            </button>
+          </div>
         </div>
         <div className="login-signup-v2-disclaimer">
-          By signing up you agree to our Terms of Use and 
-          <a href="#">Privacy Policy.</a>
+          <span>
+              By signing up you agree to our{' '}
+              <a target="_blank" rel="noopener noreferrer" href="/terms">
+              <span style={{color:'#9ED90D'}}> Terms of Use </span>
+               </a>
+               and  
+              <a target="_blank" rel="noopener noreferrer" href="/privacy">
+              <span style={{color:'#9ED90D'}} >Privacy Policy </span>
+              </a>
+               . 
+          </span>
         </div>
       </div>
     </div>
