dd12bf585a1a5e72c0156888d5e713cab5c31c96
MuhammadEssabawag <essahafe60003@gmail.com>
2024-08-14 17:03:17 +0500

    SelectCategory Added

--- src/Pages/SelectCategory/SelectCategory.jsx
+++ src/Pages/SelectCategory/SelectCategory.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './SelectCategory.css';
+import { useNavigate } from 'react-router-dom';
 
 function SelectCategory() {
+
+  const navigate = useNavigate();
+
+    const goToLoginSignuppage = () => {
+    navigate('/loginsignup');
+  };
   return (
     <div className="select-category-container">
       <div className="select-category-box">
@@ -10,13 +17,13 @@ function SelectCategory() {
         </div>
         <div className="login-signup-v2">
           <div className="login-signup-v2-button-first">
-          <button className="css-u25o92">
+          <button  onClick={goToLoginSignuppage} className="css-u25o92">
               <span className='labour'>Labour</span>
             </button> 
           </div>
           <div className="login-signup-v2-buttons">
-          <button className="css-1vj7isi">
-              <span >Customer</span>
+          <button onClick={goToLoginSignuppage} className="css-1vj7isi">
+              <span  >Customer</span>
             </button>
           </div>
          
