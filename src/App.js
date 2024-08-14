1ed604fb8d6d9095712627439a9dfa2195502f97
MuhammadEssabawag <essahafe60003@gmail.com>
2024-08-14 17:04:20 +0500

    ImportSignupmodule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import UserInterface from './components/UserInterface/UserInterface';
 import SelectCategory from './Pages/SelectCategory/SelectCategory';
+import LoginSignup from './Pages/Login-Signup/Login-Signup';
+import Signup from './Pages/Signup/Signup';
+import Login from './Pages/Login/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -10,6 +13,13 @@ function App() {
       <Routes>
         <Route path="/" element={<UserInterface />} />
         <Route path="/category" element={<SelectCategory />} />
+        <Route path="/" element={<SelectCategory/>} />
+        <Route path="/loginsignup" element={<LoginSignup />} />
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+
+        
       </Routes>
     </Router>
   );
